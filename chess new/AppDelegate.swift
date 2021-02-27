8b1f9e695294d68b397f0ed2e933393bb9638648
Maxim Meyer <meyer.maxim@t-online.de>
2021-02-27 07:24:13 +0100

    Initial Commit

---
+++ chess new/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  chess new
+//
+//  Created by Maxim Meyer on 27.02.21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
